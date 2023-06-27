9d42329dddbbdf1aa835ab39d5a8169a408e29f0
Svet_lana_17 <ocelot_17@mail.ru>
2023-06-27 23:13:14 +0600

    добавлено накручивание чисел

--- src/modules/abouttCompany.js
+++ src/modules/abouttCompany.js
@@ -2,12 +2,22 @@ import { animate } from "./helpers";
 const abouttCompany = () => {
     const numSection = document.querySelector('.num-section .row ');
     const elements = numSection.querySelectorAll('.col-sm-3');
+    const elementsValue = numSection.querySelectorAll('.push30');
 
+    console.log(elementsValue);
+    let currentCount = 0;
+
+    //elementsValue[0].textContent = currentCount;
+    //elementsValue[0].classList.remove('visible-xs');
     elements.forEach(el => {
         el.style.display = 'none';
     });
 
-    let an = setTimeout(function () {
+    const deleteInvisible = () => {
+        elementsValue.forEach(el => { el.classList.remove('visible-xs'); })
+    };
+
+    const animateBlock = () => {
         elements.forEach(el => {
             animate({
                 duration: 1000,
@@ -20,14 +30,48 @@ const abouttCompany = () => {
                 }
             });
         });
+    };
+
+    let an = setTimeout(function () {
+        animateBlock();
+        deleteInvisible();
     }, 2000)
 
-    window.onload = () => {
 
-        an();
+    const countOfOrder = (i, stopCount, text) => {
+        currentCount = 1;
+        const countInInterval = setInterval(() => {
+            currentCount += 1;
+            elementsValue[i].textContent = currentCount;
+            //console.log(elementsValue[0].textContent);
 
-
+            if (elementsValue[i].textContent >= stopCount) {
+                clearInterval(countInInterval);
+                elementsValue[i].textContent = text;
+            }
+        }, 15);
     };
+    /*  const countOfOrder = () => {
+         currentCount = 1;
+         const countInInterval = setInterval(() => {
+             currentCount += 1;
+             elementsValue[0].textContent = currentCount;
+             console.log(elementsValue[0].textContent);
+ 
+             if (elementsValue[0].textContent >= 75) {
+                 clearInterval(countInInterval);
+             }
+         }, 15);
+     }; */
+
+    window.onload = () => {
+        an;
+        countOfOrder(0, 75, 75);
+        countOfOrder(1, 15, 'по сей день');
+        countOfOrder(2, 60, '60 минут');
+        countOfOrder(3, 101, 101);
+    };
+
 
 
     console.log(elements);
